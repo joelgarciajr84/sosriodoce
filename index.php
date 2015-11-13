484e25d8507b365bf678cb8a6281a65d8e6da50f
Joel Garcia Jr <joel.garciajr84@gmail.com>
2015-11-13 11:13:20 -0200

    versao nova

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <meta charset="utf-8">
-        <title>SOS Rio Doce</title>
+        <title>S.O.S Rio Doce</title>
         <link rel="stylesheet" href="style.css" media="screen" charset="utf-8">
         <div id="fb-root"></div>
 <script>
@@ -18,25 +18,21 @@
 </script>
     </head>
     <body>
-        <h2 align="center">SOS RIO DOCE</h2>
-        <div class="full">
-
-
-            <div class="right">
-                <h2>Twitter #sosriodoce</h2>
-                <a class="twitter-timeline" href="https://twitter.com/hashtag/sosriodoce" data-widget-id="664991958511828992">sosriodoce Tweets</a>
-        <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
-            </div>
+        <h1 align="center">S.O.S RIO DOCE</h1>
+        <div align="center" class="full">
 
             <div class="left">
-                <h2>Instagram #sosriodoce</h2>
-
-                <iframe src="http://widget.websta.me/in/tag:sosriodoce/?s=200&w=2&h=3&b=1&p=5" allowtransparency="true" frameborder="0" scrolling="no" style="border:none;overflow:hidden;width:430px; height: 645px" ></iframe> <!-- websta - web.stagram.com -->
-
+                <a class="twitter-timeline" href="https://twitter.com/hashtag/sosriodoce" data-widget-id="664991958511828992">sosriodoce Tweets</a>
+                <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
             </div>
 
-            <div align="center" style="width:800px; margin:0 auto;"class="fb-comments" data-href="http://sosriodoce.herokuapp.com/" data-width="900" data-numposts="10"></div>
-        </div>
+<div class="right">
+    <div align="center" style="width:800px; margin:0 auto;"class="fb-comments" data-href="http://sosriodoce.herokuapp.com/" colorscheme="dark"
+data-width="600" data-numposts="10"></div>
+</div>
+</div>
+
+
 
     </body>
 </html>
