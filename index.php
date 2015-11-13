6c307e9cf11d1cb59222721f386571fd91ef0375
Joel Garcia Jr <joel.garciajr84@gmail.com>
2015-11-13 01:19:11 -0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>SOS Rio Doce</title>
+        <link rel="stylesheet" href="style.css" media="screen" charset="utf-8">
+        <div id="fb-root"></div>
+<script>
+
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v2.5&appId=183952811658248";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+</script>
+    </head>
+    <body>
+        <h2 align="center">SOS RIO DOCE</h2>
+        <div class="full">
+
+
+            <div class="right">
+                <h2>Twitter #sosriodoce</h2>
+                <a class="twitter-timeline" href="https://twitter.com/hashtag/sosriodoce" data-widget-id="664991958511828992">sosriodoce Tweets</a>
+        <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+            </div>
+
+            <div class="left">
+                <h2>Instagram #sosriodoce</h2>
+
+                <iframe src="http://widget.websta.me/in/tag:sosriodoce/?s=200&w=2&h=3&b=1&p=5" allowtransparency="true" frameborder="0" scrolling="no" style="border:none;overflow:hidden;width:430px; height: 645px" ></iframe> <!-- websta - web.stagram.com -->
+
+            </div>
+
+            <div align="center" style="width:800px; margin:0 auto;"class="fb-comments" data-href="http://sosriodoce.herokuapp.com/" data-width="900" data-numposts="10"></div>
+        </div>
+
+    </body>
+</html>
